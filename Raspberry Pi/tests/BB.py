755f7dbc4c46eb00a15ef343a52831891e8735cf
mcc1113 <88260559+mcc1113@users.noreply.github.com>
2021-08-01 03:48:57 -0600

    Add files via upload

--- Raspberry Pi/tests/BB.py
+++ Raspberry Pi/tests/BB.py
@@ -6,13 +6,14 @@ GPIO.setmode(GPIO.BCM)
 
 # choose GPIO 17 for the button
 button = 17
+GPIO.setup(button,GPIO.OUT)
 
 # choose GPIO 13 for the buzzer
 buzzer = 13
 GPIO.setup(buzzer,GPIO.OUT)
 
 for ii in range(0,10):
-    print(GPIO.input())
+    print(GPIO.input(button))
     # Flash
     GPIO.output(buzzer,GPIO.HIGH)
     time.sleep(1)
