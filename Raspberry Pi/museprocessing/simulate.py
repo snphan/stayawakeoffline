c1faf8f1aa9671ae8e8367a82d52324b0288df74
snphan <snphan@ualberta.ca>
2021-08-01 10:44:11 -0600

    add the overlaping window processing function

--- Raspberry Pi/museprocessing/simulate.py
+++ Raspberry Pi/museprocessing/simulate.py
@@ -50,10 +50,55 @@ def ba_static_window(time, eeg):
     return times, ba_ratios
 
 
-def ba_moving_window(time, eeg, step, window_len):
-    '''ba =beta/alpha ratio. Process the data using the moving window method'''
-    pass
+def ba_overlap_window(time, eeg, step, window_len):
+    '''ba =beta/alpha ratio. Process the data using the sliding (overlapping) window method
+
+    Params
+    ------
+    time: List(float)
+        the full time array for the data set
+    eeg: List(float)
+        a single channel from the dataset
+    step: int
+        how much to advance the window each time
+    window_len: int
+        the width of the window in number of samples
+
+    Returns
+    -------
+    times: List(float)
+        the start times of each window
+    ba_ratios: List(float)
+        the ba_ratios at each overlapping window
+    '''
     
+    chunked_time = []
+    chunked_eeg = []
+    for i in range(0, len(eeg),step):
+        if i+window_len >= len(eeg):
+            # Last time through
+            chunked_eeg.append(eeg[i:len(eeg)])
+            chunked_time.append(time[i:len(eeg)])
+            break
+        chunked_eeg.append(eeg[i:i+window_len]) 
+        chunked_time.append(time[i:i+window_len])
+
+    chunked_freqs = []
+    chunked_magnitudes = []
+    # FFT all of the chunks
+    for i in range(len(chunked_time)):
+        freq, magnitude = prep.format_fft_data(chunked_eeg[i])
+        chunked_freqs.append(freq)
+        chunked_magnitudes.append(magnitude)
+
+    # Find the beta/alpha ratios for all of the chunks
+    ba_ratios = [prep.calc_beta_alpha_ratio(chunked_freqs[i], chunked_magnitudes[i]) for i in range(len(chunked_freqs))]
+
+    # The time of b/a will be taken as the first element of each chunk
+    times = [chunked_time[i][0] for i in range(len(chunked_time))]
+
+    return times, ba_ratios
+
 
     
 
@@ -80,7 +125,8 @@ if __name__ == "__main__":
 
     # Process the second channel only:
     eeg_times, channel_2 = list(data['Time']), list(data['Channel 2'])
-    times, ba_ratios = ba_static_window(eeg_times, channel_2)
+
+    times, ba_ratios = ba_overlap_window(eeg_times, channel_2, 50, 200)
     plt.plot(times, ba_ratios)
     plt.xlabel('Time (s)')
     plt.ylabel('BA_ratios')
