ad049695ea6f2675e9468d43d3caa0d8c5f7b03e
mcc1113 <88260559+mcc1113@users.noreply.github.com>
2021-07-31 21:59:45 -0600

    Add files via upload

--- Raspberry Pi/tests/led.py
+++ Raspberry Pi/tests/led.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 # pin numbering
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # choose pin 11 (GPIO 17) for the LED
-led = 11
+led = 17
 GPIO.setup(led,GPIO.OUT)
 
 # Flash
