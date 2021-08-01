326bb0ac24b970dde9192fd7f6229bb61d4a4853
booyah990 <booyah990@gmail.com>
2021-07-31 20:54:52 -0600

    Update main.py

--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -1,5 +1,24 @@
-'''We should be able to run this code from the 
-raspberry pi and it should just keep looping
-'''
+lowerBound = 100
+upperBound = 200
+endFlag = False
 
+def processData(#bluetooth params passed????):
+    #process data from muse
+    return 150 #returns data from muse as single or tuples of frequencies
 
+def main():
+    #bluetooth connection stuff/lib
+    
+    while(True):
+        while endFlag == False:
+
+            curr = processData()
+
+            if curr < lowerBound or curr > upperBound:
+                #make beep sound
+
+            #update End flag via GPIO/button
+            
+            #optional - delay timer for inner loop
+            
+        #optional - delay timer for outer loop
