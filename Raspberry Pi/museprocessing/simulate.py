aa60723024a0215573d74ba9388433f3c943cc46
snphan <snphan@ualberta.ca>
2021-08-01 00:29:56 -0600

    add chunking function into simulate.py

---
+++ Raspberry Pi/museprocessing/simulate.py
@@ -0,0 +1,9 @@
+'''Functions for simulating collected data
+'''
+
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
