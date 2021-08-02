c944f4609ac2e8bcb46b0d00e510a0fea94044f5
booyah990 <booyah990@gmail.com>
2021-08-01 20:06:15 -0600

    please work

--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -1,10 +1,11 @@
 from uvicmuse.MuseWrapper import MuseWrapper as MW
 import asyncio
 import time
+import RPi.GPIO as GPIO
 
 lowerBound = 100
 upperBound = 200
-endFlag = False
+endFlag = 0
 
 def processData(chOne, chTwo, chThree, chFour, ref, timeSt): #bluetooth params passed????
     #process data from muse
@@ -12,6 +13,8 @@ def processData(chOne, chTwo, chThree, chFour, ref, timeSt): #bluetooth params p
     #returns data from muse as single or tuples of frequencies
 
 def main():
+    GPIO.cleanup()
+
     #bluetooth connection stuff/lib start--------------->
     loop = asyncio.get_event_loop()
     M_wrapper = MW (loop = loop,
@@ -24,9 +27,28 @@ def main():
     
     #<------end
     
+    
+    #GPIO set up start -------------------------->
+    GPIO.setmode(GPIO.BCM)
+    led = 27
+    GPIO.setup(led,GPIO.OUT)
+    button = 17
+    GPIO.setup(button,GPIO.OUT)
+    buzzer = 13
+    GPIO.setup(buzzer,GPIO.OUT)
+    pwm = GPIO.PWM(buzzer, 3500)
+    GPIO.output(led,GPIO.LOW)
+    
+    #<-------------end
+    
+    
     while(True):
-        while endFlag == False:
-            time.sleep(3)
+        if GPIO.input(button) == 1:
+            endFlag = 0
+            GPIO.output(led,GPIO.HIGH)
+            time.sleep(2)
+        while endFlag == 0:
+            time.sleep(3) 
             EEG_data = M_wrapper.pull_eeg()
             chOne = []
             chTwo = []
@@ -46,14 +68,20 @@ def main():
             curr = processData(chOne, chTwo, chThree, chFour, ref, timeSt)
 
             if curr < lowerBound or curr > upperBound:
-                print("xd")
+                pwm.start(50)
+                time.sleep(0.5)
+                pwm.ChangeDutyCycle(0)
                 #make beep sound
 
-            #update End flag via GPIO/button
-            
-            #optional - delay timer for inner loop
-            
-        #optional - delay timer for outer loop
+            if GPIO.input(button) == 1:
+                if endFlag == 0:
+                    endFlag = 1
+                    GPIO.output(led,GPIO.LOW)
+                    time.sleep(2)
+                else:
+                    endFlag = 0
+                    GPIO.output(led,GPIO.HIGH)
+                    time.sleep(2)
 
 
 main()
