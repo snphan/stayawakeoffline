cce7db8119727f481b1865bfc477da24236d6da1
mcc1113 <88260559+mcc1113@users.noreply.github.com>
2021-08-01 19:40:23 -0600

    Add files via upload

--- Raspberry Pi/tests/BB2.py
+++ Raspberry Pi/tests/BB2.py
@@ -4,6 +4,10 @@ import time
 # pin numbering
 GPIO.setmode(GPIO.BCM)
 
+# choose GPIO 27 for the led
+led = 27
+GPIO.setup(led,GPIO.OUT)
+
 # choose GPIO 17 for the button
 button = 17
 GPIO.setup(button,GPIO.OUT)
@@ -13,14 +17,21 @@ buzzer = 13
 GPIO.setup(buzzer,GPIO.OUT)
 pwm = GPIO.PWM(buzzer, 3500)
 
+GPIO.output(led,GPIO.LOW)
+flag = GPIO.input(button)
+print(type(flag))
 
-for ii in range(0,10):
-    print(GPIO.input(button))
+for ii in range(0,6):
+    flag = GPIO.input(button)
+    if flag:
+        GPIO.output(led,GPIO.HIGH)
+    else:
+        GPIO.output(led,GPIO.LOW)   
     # Flash
     pwm.start(50)
-    time.sleep(1)
+    time.sleep(5)
     pwm.ChangeDutyCycle(0)
-    time.sleep(0.2)
+    time.sleep(0.1)
 
 
 GPIO.cleanup()
